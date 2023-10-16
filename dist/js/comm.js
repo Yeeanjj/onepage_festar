13105c4a76ab388ba9f9ba0dda5224bedb69473e
Yeeanj <sesju0527@gamil.com>
2023-10-16 18:03:18 +0900

    localevent_swiper + bg img

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,1 +1,13 @@
 AOS.init();
+var swiper = new Swiper(".local_event", {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
